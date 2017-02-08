a02ba264a4d726476dd0077db6312c0139510e81
namastayinschool <namastayinschool@users.noreply.github.com>
2017-02-08 10:29:02 -0400

    Implemented linear search algorithm

--- search.cpp
+++ search.cpp
@@ -14,6 +14,18 @@ using namespace std;
 */
 int linearSearch(auto data, auto key);//prototype
 
+int linearSearch(auto data, auto key)
+{
+	for (int i=0; i < data.size(); i++)
+	{
+		if (data[i] == key)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
 
 int main()
 {
